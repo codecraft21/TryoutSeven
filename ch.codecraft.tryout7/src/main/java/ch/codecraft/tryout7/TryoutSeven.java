e5e53cd4a54166f454b2b2abac0754851fa0ae5e
Chris <chris@codecraft.ch>
2011-12-03 19:07:03 +0100

    first steps with Java7 with Eclipse on Linux and Git repo

---
+++ ch.codecraft.tryout7/src/main/java/ch/codecraft/tryout7/TryoutSeven.java
@@ -0,0 +1,26 @@
+package ch.codecraft.tryout7;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Setting up Oracle Java 7 and Eclipse Indigo on Linux.
+ * 
+ * @author chris
+ */
+public class TryoutSeven {
+
+	public static void main(String[] args) {
+		Map<String,String> map = new HashMap<>();
+		map.put("key", "value");
+		switch (map.get("key")) {
+		case "value":
+			System.out.println("Hello, Java 7");
+			break;
+
+		default:
+			throw new RuntimeException();
+		}
+	}
+
+}
